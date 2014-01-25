931d8d92fb429b5abc26f7a9f8dd55f8d667e690
niaoer <niaoer@niaoer.org>
2014-01-25 12:21:28 +0800

    v 1.0.13
    移除底部主题链接

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</section><!-- end main section -->
 	<footer id="footer">
 		<div class="left">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('All rights reserved', 'picochic'); ?>.</div>
-		<div class="right"><a href="<?php echo esc_url('http://picomol.de/picochic/'); ?>" rel="nofollow"><?php _e('Design by picomol', 'picochic');?></a>. <?php _e('Powered by ', 'picochic'); ?><a href="<?php echo esc_url('http://www.wordpress.org'); ?>">WordPress</a>.</div>
+		<div class="right"><?php _e('Design by picomol', 'picochic');?>. <?php _e('Powered by ', 'picochic'); ?><a href="<?php echo esc_url('http://www.wordpress.org'); ?>">WordPress</a>.</div>
 	</footer>
 </div>
 		<?php wp_footer(); ?>
